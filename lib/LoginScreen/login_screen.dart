75e93fca1511c58a3c13048ce2b7c9372fa880a0
Samiul Basir <samiul45basir@gmail.com>
2025-10-24 16:17:54 +0600

    new code added

--- lib/LoginScreen/login_screen.dart
+++ lib/LoginScreen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/HomeScreen/home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,7 +7,18 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(backgroundColor: Colors.lightBlueAccent),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
+          },
+          child: Icon(Icons.arrow_forward),
+        ),
+      ),
     );
   }
 }
