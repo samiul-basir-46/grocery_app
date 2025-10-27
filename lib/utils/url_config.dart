199b92dbd3910f900b67a2572caceafc47185287
samiul <samiul45basir@gmail.com>
2025-10-28 03:22:18 +0600

    url add

---
+++ lib/utils/url_config.dart
@@ -0,0 +1,6 @@
+class Url {
+  static const String baseUrl = "https://foodsflow.netlify.app/api";
+  static const String regUrl = "$baseUrl/auth/register";
+  static const String loginUrl = "$baseUrl/auth/login";
+}
+
